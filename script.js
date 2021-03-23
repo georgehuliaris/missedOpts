0abc09b5c431d1f97703b73da0bd3cf442167a03
alligatoraustin <austin_smith07@hotmail.com>
2021-03-23 13:57:20 -0500

    2nd commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var form = document.getElementById("form-id");
+
+document.getElementById("your-id").addEventListener("click", function () {
+form.submit();
+if (submit.isPressed) {
+    console.log("Clicked!");
+}
+});
