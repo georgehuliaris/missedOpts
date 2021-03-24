e8e2a3eb61a21260f8edf5ddaf64d3b80bb0bca4
alligatoraustin <austin_smith07@hotmail.com>
2021-03-24 12:53:39 -0500

    commit 3

--- Assets/script.js
+++
@@ -1,8 +0,0 @@
-var formSubmit = document.getElementById("myForm");
-
-document.getElementById("myForm").addEventListener("click", function (event) {
-event.preventDefault();
-
-if(this.click)
-console.log("clicked!");
-});
