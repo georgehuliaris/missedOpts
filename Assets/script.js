fccfa96d3b490f8316c1a3f09358e5c7921a78f0
denis salvino <denis.salvino23@gmail.com>
2021-03-24 15:23:47 -0500

    created a toggle switch between crypto and stock forms

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,51 @@
-var formSubmit = document.getElementById("myForm");
-// this is the form submit button
-document.getElementById("myForm").addEventListener("click", function (event) {
-formSubmit.submit();
+// function toggleSwitch() {
+// var element = document.getElementById('pageToggle');
+// element.classList.toggle('cryptoMode');
+// }
 
+// var input = document.querySelector('#pageToggle');
+
+// input.addEventListener('change',function(){
+//     if(this.checked) {
+//         console.log('crypto');
+//     } else {
+//         console.log('stock');
+//     }
+// });
+// toggles between crypto mode and stock mode 
+$('input[type="checkbox"]').click(function(){
+    if($(this).is(":checked")){
+       $('body').toggleClass('cryptoMode');
+    }
+    else if($(this).is(":not(:checked)")){
+       console.log('deactivated');
+    }
 });
 
-function bitfinData() {
-    URL: "https://api-pub.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist";
-}
+// var formSubmit = document.getElementById("myForm");
+// // this is the form submit button
+// document.getElementById("myForm").addEventListener("click", function (event) {
+// formSubmit.submit();
 
-function iexData(){
-    var URL = "https://cloud.iexapis.com/v1?token=pk_95cd7c4ff00744a5a178e9fecad2319e";
+// });
 
-}
+// function bitfinData() {
+//     URL: "https://api-pub.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist";
+// }
 
-let myChart = document.getElementById("myChart").getContext("2D");
-let crypto = new chart(myChart, {
+// function iexData(){
+//     var URL = "https://cloud.iexapis.com/v1?token=pk_95cd7c4ff00744a5a178e9fecad2319e";
 
-    type: 'line',
-    data:{
-        labels:[],
-        datasets:[],
-    },
-        options:{};
+// }
 
-});
+// let myChart = document.getElementById("myChart").getContext("2D");
+// let crypto = new chart(myChart, {
+
+//     type: 'line',
+//     data:{
+//         labels:[],
+//         datasets:[],
+//     },
+//         options:{};
+
+// });
