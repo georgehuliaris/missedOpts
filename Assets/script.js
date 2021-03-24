64a5255fa02c3d9f2a02faed668a727fbb4ff2c6
George Huliaris <georgehuliaris@me.com>
2021-03-24 14:32:22 -0500

    added function for average

--- Assets/script.js
+++ Assets/script.js
@@ -25,3 +25,23 @@ let crypto = new chart(myChart, {
         options:{},
 
 });
+
+// This is just a function to average the open and close price together
+
+// let a = ${#open};
+// let b = ${#close};
+// let c = ${#open};
+// let d = ${#close};
+
+// function average(a, b) {
+//     // force the input as numbers *1
+// return ((a*1 + b*1) /2);
+// }
+
+// document.getElementById("averageButton").onclick = function (){
+//         var a = document.getElementById("userInput1").value;
+//     var b = document.getElementById("userInput2").value;    		
+//     // pass the numbers to the average function!
+// alert(average(a,b));
+// }
+
