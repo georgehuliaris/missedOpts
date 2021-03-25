e2da2e12d65afebf5558cdffefb77ca8e14112a9
Rodrigo Galicia <rodrigo_galicia95@yahoo.com>
2021-03-25 10:14:56 -0500

    Morning JS update

--- Assets/script.js
+++ Assets/script.js
@@ -25,3 +25,48 @@ let crypto = new chart(myChart, {
         options:{},
 
 });
+document.getElementById('average').onclick = async function () {
+
+    var url = "https://cloud.iexapis.com/v1?token=pk_95cd7c4ff00744a5a178e9fecad2319e";
+
+    var todayAvg;
+    console.log(todayAvg)
+
+    await $.get(url, function (data) {
+        console.log('todays data', data)
+        var open = data.open;
+        var close = data.close;
+
+        todayAvg = average(open, close);
+});
+
+    console.log('todayAvg', todayAvg)
+
+
+var historicalAvg;
+var endOfDayURL = "https://cloud.iexapis.com/v1?token=pk_95cd7c4ff00744a5a178e9fecad2319e";
+
+await $.get(endOfDayURL, function (data) {
+
+    const lastData = data[data.length - 1];
+    var open = lastData.open;
+    var close = lastData["close data"];
+
+
+    historicalAvg = average(open, close);
+});
+
+var change = todayAvg - historicalAvg;
+
+document.getElementById('change-result').innerHTML = change;
+
+comsole.log('historicalAvg', historicalAvg)
+comsole.log('change', change)
+
+}
+
+function average(a, b) {
+    console.log(a, b)
+    return (a + b) / 2;
+
+}
