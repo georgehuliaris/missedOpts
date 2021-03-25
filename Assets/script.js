d0cc1dab4435884a9e44ee02cade624c7cad1754
Rodrigo Galicia <rodrigo_galicia95@yahoo.com>
2021-03-25 12:53:35 -0500

    testing change

--- Assets/script.js
+++ Assets/script.js
@@ -56,8 +56,50 @@ formSubmit.submit();
 // function bitfinData() {
 //     URL: "https://api-pub.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist";
 // }
+// This is just a function to average the open and close price together
 
-document.getElementById('average').onclick = async function () {
+// let a = ${#open};
+// let b = ${#close};
+// let c = ${#open};
+// let d = ${#close};
+
+// function average1(a, b) {
+//     // force the input as numbers *1
+// return ((a*1 + b*1) /2);
+// }
+
+// document.getElementById("averageButton").onclick = function (){
+//         var a = document.getElementById("userInput1").value;
+//     var b = document.getElementById("userInput2").value;    		
+//     // pass the numbers to the average function!
+// return(average1(a,b));
+// }
+
+// function average2(c, d) {
+//     // force the input as numbers *1
+// return ((c*1 + d*1) /2);
+// }
+
+// document.getElementById("averageButton").onclick = function (){
+//         var a = document.getElementById("userInput1").value;
+//     var b = document.getElementById("userInput2").value;    		
+//     // pass the numbers to the average function!
+// return(average2(c,d));
+// }
+
+// function averageDone(average1, average2) {
+//     // force the input as numbers *1
+// return ((average1*1 + average2*1) /2);
+// }
+
+// document.getElementById("averageButton").onclick = function (){
+//         var a = document.getElementById("userInput1").value;
+//     var b = document.getElementById("userInput2").value;    		
+//     // pass the numbers to the average function!
+// return(averageDone(average1,average2));
+// }
+
+document.getElementById('Ticker').onclick = async function () {
 
     var url = "https://cloud.iexapis.com/v1?token=pk_95cd7c4ff00744a5a178e9fecad2319e";
 
@@ -102,49 +144,5 @@ function average(a, b) {
     return (a + b) / 2;
 
 }
-
-
-// This is just a function to average the open and close price together
-
-// let a = ${#open};
-// let b = ${#close};
-// let c = ${#open};
-// let d = ${#close};
-
-// function average1(a, b) {
-//     // force the input as numbers *1
-// return ((a*1 + b*1) /2);
-// }
-
-// document.getElementById("averageButton").onclick = function (){
-//         var a = document.getElementById("userInput1").value;
-//     var b = document.getElementById("userInput2").value;    		
-//     // pass the numbers to the average function!
-// return(average1(a,b));
-// }
-
-// function average2(c, d) {
-//     // force the input as numbers *1
-// return ((c*1 + d*1) /2);
-// }
-
-// document.getElementById("averageButton").onclick = function (){
-//         var a = document.getElementById("userInput1").value;
-//     var b = document.getElementById("userInput2").value;    		
-//     // pass the numbers to the average function!
-// return(average2(c,d));
-// }
-
-// function averageDone(average1, average2) {
-//     // force the input as numbers *1
-// return ((average1*1 + average2*1) /2);
-// }
-
-// document.getElementById("averageButton").onclick = function (){
-//         var a = document.getElementById("userInput1").value;
-//     var b = document.getElementById("userInput2").value;    		
-//     // pass the numbers to the average function!
-// return(averageDone(average1,average2));
-// }
 
 
